d0c519cd6a61468b6a689f946025f3e0f8b26513
Bernardo-Franco <bernard.f651@gmail.com>
2024-03-05 03:33:16 -0500

    front funtionality complete, TODO:sockets for real time implementation

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
